5cf054dd7f3f571e6ef609babab873553f3c3970
ruusty <russell@holliday.id.au>
2019-03-17 23:39:57 +1100

    refactor

--- md2html/Public/Convert-Markdown2Html.ps1
+++ md2html/Public/Convert-Markdown2Html.ps1
@@ -1,100 +1,122 @@
-﻿function Convert-Markdown2Html
+﻿<#
+  .SYNOPSIS
+    Convert Markdown files to Html
+  
+  .DESCRIPTION
+    Convert Markdown files to Html
+  
+  .PARAMETER Path
+    Wildcard filespec of Markdown files
+  
+  .PARAMETER LiteralPath
+    A description of the LiteralPath parameter.
+  
+  .PARAMETER Recurse
+    Recurse directories for Markdown files using Path
+  
+  .PARAMETER HighlightWeb
+    A description of the HighlightWeb parameter.
+  
+  .PARAMETER HightlightLocal
+    A description of the HightlightLocal parameter.
+  
+  .EXAMPLE
+    Convert-Markdown2Html -Path *.md -whatif
+  
+  .EXAMPLE
+    "*.md", 'README*.md', "..\Specification\*.md", "..\Specification\*.md" | Convert-Markdown2Html -verbose -recurse
+  
+  .EXAMPLE
+    "*.md", 'README*.md', "..\Specification\*.md", "..\Specification\*.md" | Convert-Markdown2Html -verbose -recurse -HighlightWeb
+  
+  .EXAMPLE
+    Convert-Markdown2Html -path "*.md", 'README*.md', "..\Specification\*.md"
+  
+  .NOTES
+    Additional information about the function.
+#>
+function Convert-Markdown2Html
 {
   [CmdletBinding(DefaultParameterSetName = 'Path',
                  SupportsShouldProcess = $true)]
   param
   (
     [Parameter(ParameterSetName = 'Path',
+               Mandatory = $false,
                ValueFromPipeline = $true,
                Position = 1)]
     [SupportsWildcards()]
     [string[]]$Path = "*.md",
     [Parameter(ParameterSetName = 'LiteralPath',
-               Mandatory = $true,
+               Mandatory = $false,
                Position = 1)]
     [Alias('PSPath')]
     [String[]]$LiteralPath = $null,
-    [Parameter(ParameterSetName = 'Path')]
     [switch]$Recurse,
-    [string]$CssFile = $CssPath
+    [Alias('Hilite')]
+    [switch]$HighlightWeb,
+    [switch]$HightlightLocal
   )
-  #region Initialize
-  $builder = New-Object Markdig.MarkdownPipelineBuilder
-  # use UseAdvancedExtensions for better error reporting
-  $pipeline = [Markdig.MarkdownExtensions]::UseAdvancedExtensions($builder).Build()
-  #endregion Initialize
   
-  $psBoundParameters.GetEnumerator() | Sort-Object Name | % { $_.key + " is " + $_.value } | out-string | write-verbose
-  
-  #for each Markdown file
-  # 1. Use MarkDig to convert the markdown to the HTML body
-  # 2. Adding the CSS and in the header
-  # 3. Create the file
-  
-  Write-Verbose -Message ("ParameterSetName:{0}" -f $PsCmdlet.ParameterSetName)
-  switch ($PsCmdlet.ParameterSetName)
+  Begin
   {
-    "Path"         { $GetChildItemArgs = @{ Path = $Path }; break }
-    "LiteralPath"  { $GetChildItemArgs = @{ LiteralPath = $LiteralPath }; break }
-  }
-  
-  $GetChildItemArgs.GetEnumerator() | Sort-Object Name | % { $_.key + " is " + $_.value } | out-string | write-verbose
-  
-  if ($Recurse)
-  {
-    $GetChildItemArgs += @{ recurse = $Recurse }
-  }
-  $files = Get-ChildItem @GetChildItemArgs -File -Exclude "*.html"
-  
-  foreach ($mdfile in $files)
-  {
-    $ext = [system.io.path]::GetExtension($mdfile.fullname)
-    if ($ext -eq ".md")
+    $IsVerbose = $false
+    if ($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent)
     {
-      $htmlFileName = [system.io.path]::ChangeExtension($mdfile.fullname, "html")
-      $Name = [system.io.path]::GetFileNameWithoutExtension($mdfile.fullname)
-      $cmdline = ('{0} ==> {1}' -f $mdfile.fullname, $htmlFileName)
-      if ($PSCmdlet.ShouldProcess(
-          "$cmdline",
-          "ConvertFrom-Markdown2Html"))
-      {
-        #Write-Verbose $($file + " ==> " + $htmlFileName)
-        $sb = New-Object System.Text.StringBuilder
-        [void]$sb.AppendLine('<!doctype html>')
-        [void]$sb.AppendLine('<html>')
-        [void]$sb.AppendLine("<!-- Generated {0} by [Markdig.Markdown]::ToHtml -->" -f $([System.DateTime]::Now))
-        [void]$sb.AppendLine("<!-- Generated from {0} -->" -f $mdfile.fullname)
-        [void]$sb.AppendLine("<head>")
-        [void]$sb.AppendLine('<meta charset="utf-8">')
-        [void]$sb.AppendLine('<meta http-equiv="x-ua-compatible" content="ie=edge">')
-        [void]$sb.AppendLine("<title>$Name</title>")
-        [void]$sb.AppendLine('<style type="text/css">')
-        if (Test-Path $CssFile) { [void]$sb.AppendLine((get-content $CssFile)) }
-        [void]$sb.AppendLine('</style></head>')
-        [void]$sb.AppendLine('<body>')
-        try
-        {
-          if ($mdfile.Length -gt 0)
-          {
-            [void]$sb.AppendLine($([Markdig.Markdown]::ToHtml($(Get-Content -Raw $mdfile.fullname), $pipeline)))
-          }
-        }
-        catch [ArgumentNullException]
-        {
-          Write-Warning $_
-          Write-Warning $_.ScriptStackTrace
-        }
-        finally
-        {
-          [void]$sb.AppendLine('</body>')
-          [void]$sb.AppendLine('</html>')
-          $sb.ToString() | out-file $htmlFileName -Encoding "UTF8"
-          $htmlFile = (Get-ChildItem -Path $htmlFileName)
-          $htmlFile.lastwritetime = $mdFile.lastwritetime
-          $htmlFile.CreationTime = $mdFile.CreationTime
-        }
+      $IsVerbose = $true
+    }
+    #region setup
+    # Get the command name
+    $CommandName = $PSCmdlet.MyInvocation.InvocationName;
+    # Get the list of parameters for the command
+    "${CommandName}: Input", (((Get-Command -Name ($PSCmdlet.MyInvocation.InvocationName)).Parameters) |
+      ForEach-Object { Get-Variable -Name $_.Values.Name -ErrorAction SilentlyContinue; } |
+      Format-Table -AutoSize @{ Label = "Name"; Expression = { $_.Name }; }, @{ Label = "Value"; Expression = { (Get-Variable -Name $_.Name -EA SilentlyContinue).Value }; }) |
+    Out-String | write-verbose
+    #endregion
+    write-verbose $('{0}-{1}' -f '$WhatIfPreference', $WhatIfPreference)
+    [string]$CssPath = $(join-path -path $MyInvocation.MyCommand.Module.ModuleBase -ChildPath $(Get-ConfigData).CssPath)
+        [string]$HighlightCssPath = $(join-path -path $MyInvocation.MyCommand.Module.ModuleBase -ChildPath $(Get-ConfigData).HighlightJsCssPath)
+    [string]$HighlightJsPath = (Get-ConfigData).HighlightJsPath
+    [string]$HighlightJsPathLocal = (Get-ConfigData).HighlightJsPathLocal
+    @('CssPath', 'HighlightCssPath', 'HighlightJsPathLocal', 'HighlightJsPath') | Get-Variable -ea Continue | Sort-Object -Property name -CaseSensitive | Format-Table -property name, value -autosize | Out-String -Width 80 | write-verbose
+  }
+  Process
+  {
+    Write-Verbose -Message ("ParameterSetName:{0}" -f $PsCmdlet.ParameterSetName)
+    switch ($PsCmdlet.ParameterSetName)
+    {
+      "Path" {
+        $GetChildItemArgs = @{ Path = $Path }; break
+      }
+      "LiteralPath" {
+        $GetChildItemArgs = @{ LiteralPath = $LiteralPath }; break
       }
     }
+    
+    if ($Recurse)
+    {
+      $GetChildItemArgs += @{ recurse = $Recurse }
+    }
+    
+    #$GetChildItemArgs.GetEnumerator() | Sort-Object Name | ForEach-Object { $_.key + " is " + $_.value } | out-string | write-verbose
+    
+    $CMArgs = @{
+      CssPath          = $CssPath
+      HighlightCssPath = $HighlightCssPath
+      Verbose          = $IsVerbose
+      whatif           = $WhatIfPreference
+    }
+    
+    if ($HighlightWeb)
+    {
+      $CMArgs.HighlightJsPath = $HighlightJsPath;
+    }
+    if ($HightlightLocal)
+    {
+      $CMArgs.HighlightJsPath = $HighlightJsPathLocal;
+    }
+    Get-ChildItem @GetChildItemArgs -File -Exclude "*.html" | Sort-Object -unique | Convert-Markdown @CMArgs
   }
 }
 
